758637eec505f8614a086fc99f85068f3554f47b
Sivaprasath Mani <>
2023-11-20 15:15:21 +0530

    Added Clear and Speech Button

--- app/src/main/java/com/app/test/search/place/ui/searchlist/SearchHomeScreen.kt
+++ app/src/main/java/com/app/test/search/place/ui/searchlist/SearchHomeScreen.kt
@@ -13,11 +13,14 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -27,8 +30,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
@@ -126,6 +130,7 @@ fun SearchView(
                     .size(24.dp)
             )
         },
+        trailingIcon = if (searchViewModel.getSearchText().isEmpty()) speechIconView else clearIconView,
         singleLine = true,
         shape = RectangleShape,
         keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
@@ -133,6 +138,31 @@ fun SearchView(
             onSearch = { searchViewModel.getHotelSearch(searchString) },
         )
     )
+}
+
+val clearIconView = @Composable {
+    IconButton(
+        onClick = { },
+    ) {
+        Icon(
+            Icons.Default.Clear,
+            contentDescription = "",
+            tint = Color.Black
+        )
+    }
+}
+
+val speechIconView = @Composable {
+    val context = LocalContext.current
+    IconButton(
+        onClick = { },
+    ) {
+        Icon(
+            Icons.Default.AccountCircle,
+            contentDescription = "",
+            tint = Color.Black
+        )
+    }
 }
 
 @Composable
